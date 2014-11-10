36974f75ecc386f7a23796de411d57ec4a59ce1b
Melanie Heisey <hamnox@bangup>
2014-11-10 08:38:36 -0700

    Room Class init + Location Dictionary set

--- text_game.py
+++ text_game.py
@@ -1,15 +1,6 @@
 # import 
 
-map_size = [10,10]
-
-# change this to a for loop to make dictionary, generate a room.
-Locations = {[row[i] for row in myArray] 
-          for i in range(len(myArray))}
-
-locations["Inventory"] = []
-# the last location is a list.
-print "Welcome to my world :) "
-input()
+map_size = (10,10)
 
 def move():
 	# get location
@@ -26,14 +17,14 @@ def list():
 	print locations.append[-1] # inventory
 	return
 
+# add a parser!
 class Room:
-	def __init__(self,name,items,location):
+	def __init__(self,name,items=[]):
 		self.name = name # attribute name/description
 		self.items = [] # attribute items
 		self.items.extend(items)
-		self.location = location # init assign_location
-	def self.look(item) # THIS DOESN"T WORK WTF https://docs.python.org/2/tutorial/classes.html
-		if item in self.items
+	def look(self,item): # THIS DOESN"T WORK WTF https://docs.python.org/2/tutorial/classes.html
+		if item in self.items:
 			return "ITEM DESCRIPTION" # TO BE CHANGED WHEN I MAKE AN ITEM
 		else:
 			return "FAIL"
@@ -43,7 +34,7 @@ class Room:
 	# function put
 	# function get_location
 
-
+"""
 class Item:
 	# add match names to dictionary
 	# attribute name/description
@@ -67,5 +58,20 @@ class Character:
 			room.put(item)
 		except:
 			print "That didn't work dummy!"
-	#function inventory
+	#function inventory"""
 	
+	
+# change this to a for loop to make dictionary, generate a room.
+Locations = {}
+#[row[i] for row in map_size[0]]
+#          for i in range(len(map_size[1]+1)):""}
+
+print range(1,map_size[0]+1)
+for x in range(1,map_size[0]+1):
+  for y in range(1,map_size[1]+1):
+    Locations[(x,y)] = Room("Room"+str(x)+"-"+str(y))
+
+Locations["Inventory"] = []
+# the last location is a list.
+print "Welcome to my world :) "
+input()
