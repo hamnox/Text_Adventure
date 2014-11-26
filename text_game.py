c5b466991b453863e68472c232493d9ba4d9855b
Melanie Heisey <hamnox@macman>
2014-11-26 05:19:24 -0700

    Mapping
    made the mapping work, and changed the style of object

--- text_game.py
+++ text_game.py
@@ -1,77 +1,103 @@
-# import 
-
-map_size = (10,10)
+#import pudb #this is for debugging
+ # import 
+# THIS DOESN"T WORK WTF https://docs.python.org/2/tutorial/classes.html
 
 def move():
-	# get location
-	# check location_exists
-	# check walls
-	# move location
-	print "You have moved!"
-	return
+    # get location
+    # check location_exists
+    # check walls
+    # move location
+    print "You have moved!"
+    return
 
 def take():
-	return
+    return
 
 def list():
-	print locations.append[-1] # inventory
-	return
+    print locations.append[-1] # inventory
+    return
 
+"""def invertDict(dictionary):
+    return dict([v,k] for k,v in mydict.iteritems())
+    """
 # add a parser!
-class Room:
-	def __init__(self,name,items=[]):
-		self.name = name # attribute name/description
-		self.items = [] # attribute items
-		self.items.extend(items)
-	def look(self,item): # THIS DOESN"T WORK WTF https://docs.python.org/2/tutorial/classes.html
-		if item in self.items:
-			return "ITEM DESCRIPTION" # TO BE CHANGED WHEN I MAKE AN ITEM
-		else:
-			return "FAIL"
-	
-	# function look
-	# function move
-	# function put
-	# function get_location
+class World(object):
+    def __init__(self,name,map_size=(10,10),startLocation=(1,1)):
+        # change this to a for loop to make dictionary, generate a room.
+        self.Locations = {}
+        #[row[i] for row in map_size[0]]
+        #          for i in range(len(map_size[1]+1)):""}
 
-"""
-class Item:
-	# add match names to dictionary
-	# attribute name/description
-	# attribute hidden?
-	# attribute interactable
-	# attribute moveable
-	# attribute location
-	# function interact (look up dictionary list of terms)
-	# function look
-	# function use
-	# function grok
-	
+        # print range(1,map_size[0]+1)
+        for x in range(1,map_size[0]+1):
+          for y in range(1,map_size[1]+1):
+            self.Locations[(x,y)] = Room(self,(x,y),name="Room "+str(x)+"-"+str(y))
 
-class Character:
-	# attribute status
-	# attribute location
-	def drop():
-		#match item by name list
-		try:
-			locations[-1].remove(item)
-			room.put(item)
-		except:
-			print "That didn't work dummy!"
-	#function inventory"""
-	
-	
-# change this to a for loop to make dictionary, generate a room.
-Locations = {}
-#[row[i] for row in map_size[0]]
-#          for i in range(len(map_size[1]+1)):""}
+        self.Locations["Inventory"] = Room(self,"Inventory",name="Inventory",description="Where all your stuff is kept")
+        self.Locations["Ether"] = Room(self,"Ether",name="Ether",description="The land of hidden things")
 
-print range(1,map_size[0]+1)
-for x in range(1,map_size[0]+1):
-  for y in range(1,map_size[1]+1):
-    Locations[(x,y)] = Room("Room"+str(x)+"-"+str(y))
+        # the last location is a list.
+        print "Welcome to the wonderful world of {}!".format(name)
+        self.maplinks()
+        
+    def maplinks(self): # add init=True):
+        for coord, room in self.Locations.items():
+            for direction,location_offset in [["North",(1,0)],["South",(-1,0)],["East",(1,0)],["West",(-1,0)]]:
+                try:
+                    room.addmap(direction,(coord[0]+location_offset[0],coord[1]+location_offset[1]))
+                    print direction,coord[0]+location_offset[0],coord[1]+location_offset[1]
+                except:
+#                    if isinstance(coord[0],int):
+ #                       print (coord[0]+location_offset[0],coord[1]+location_offset[1])
+                    pass
+        #someday it would be fun to allow rooms inside of rooms. But not today, PLEASE.
 
-Locations["Inventory"] = []
-# the last location is a list.
-print "Welcome to my world :) "
-input()
+class Room(object):
+    #pudb.set_trace() #this is for debugging
+    def __init__(self,world,location_key,items=[],name="Void",description="Nothing here to see."):
+        self.name = name # attribute name/description
+        self.description = description
+        self.items = [] # attribute items
+        self.items.extend(items)
+        self.world = world
+        self.location = location_key
+        self.doors = {}
+        self.directions = {}
+    
+    def look(self):
+        return self.description
+    def relocate(location): #,swap=True):
+        pass # add this later yo
+    def addmap(self,direction,location): #hidden=False):
+        self.doors[direction] = self.world.Locations[location] #make sure we test this against real locations first.
+        #TODO actually, let's just add this into world later.
+    def put(item):
+        self.items.extend(item)
+    def remove(item):
+        self.items.remove(item)
+        Map.Locations["Ether"].extend(item)
+
+class Item(object):
+    def __init__(self,name="Thing",location="Ether",description="A Thingy Thing"):
+        self.name = name
+        self.altnames = []
+        self.location = location
+        self.description = description
+
+    # add match names to dictionary
+    # attribute name/description
+    # attribute hidden?
+    # attribute interactable
+    # attribute moveable
+    # attribute location
+    # function interact (look up dictionary list of terms)
+    # function look
+    # function use
+    # function grok
+    pass
+
+    
+
+World("TextLand")
+x = raw_input("~~> ")
+print x, "indeed."
