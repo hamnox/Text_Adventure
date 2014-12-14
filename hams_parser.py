d1cd66a0a6953045f36ef304a109692670380123
hamnox <mheisey.nox@gmail.com>
2014-12-14 08:48:57 -0800

    Got some parser working kind of
    mid-morning brilliance

---
+++ hams_parser.py
@@ -0,0 +1,24 @@
+import parsley #do add this into the installer or whatever.
+import sys
+thismodule = sys.modules[__name__]
+
+def use(thing):
+    print thing.upper(), "WORKS"
+    
+x = parsley.makeGrammar("""
+toplevel = pairs+
+pairs = command:cmd ws thing:this ws? -> cmd,this
+command = use | go | custom
+use = ('use' ' the'? | 'try' 'the'?):txt -> "use"
+go = ('go' ' to'? | 'walk' ' to'?):txt -> 'goto'
+custom = ('cucumber walowitz'):txt -> 'walowitz'
+thing = ('bathroom' | 'genius')""",{}) #{"rule": function/object"}
+
+
+result = x('use the bathroom try genius').toplevel()
+
+
+#next must add a method for extending grammar
+for pair in result:
+    methodToCall = getattr(thismodule, pair[0])
+    methodToCall(pair[1])
